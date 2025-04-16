42eea5360e37d9d33d9a22ad410ac6c4a986c8a4
APIVerve Team <hello@apiverve.com>
2025-04-16 09:51:46 -0500

    Bug Fixes

---
+++ PlanetPositionsQueryOptions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class PlanetPositionsQueryOptions {
+public date date { get; set; }
+public number lat { get; set; }
+public number lon { get; set; }
+public number alt { get; set; }
+public string planet { get; set; }
+}
+
+}
